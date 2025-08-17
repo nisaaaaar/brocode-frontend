f956f516782069fee1dc71f4fbde9302afd7bbf2
nisaaaaar <nisara305@gmail.com>
2025-08-17 22:41:29 +0530

    updated socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
 // Point this to your Flask server
-// export const socket = io(import.meta.env.VITE_SERVER_URL || "http://localhost:5000", {
-export const socket = io(process.env.REACT_APP_BACKEND_URL, {
+export const socket = io("https://brocode-backend-sk08.onrender.com", {
+// export const socket = io("https://brocode-backend-sk08.onrender.com", {
   autoConnect: false,
   transports: ["websocket"]
 });
